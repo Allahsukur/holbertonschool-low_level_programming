d4cf2dde50ab0f79052a15a4c6291a5059fd5597
Allahsukur <10745@holbertontudents.com>
2025-03-30 19:37:36 +0000

    456789

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,10 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
-void print_numbers(const char *separator, const unsigned int n, ...);
+#include <stdarg.h>
+#include <stddef.h> /* for NULL */
+#include <stdio.h>  /* for printf */
+
+void print_strings(const char *separator, const unsigned int n, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
