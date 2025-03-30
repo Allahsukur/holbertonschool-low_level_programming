e438eccea4ee439bfa999045e4c0ca4cddaa0675
Allahsukur <10745@holbertontudents.com>
2025-03-30 19:35:09 +0000

    456789

--- function_pointers/variadic_functions/variadic_functions.h
+++ function_pointers/variadic_functions/variadic_functions.h
@@ -2,7 +2,8 @@
 #define VARIADIC_FUNCTIONS_H
 
 #include <stdarg.h>
-#include <stdio.h>
+#include <stddef.h> /* NULL üçün */
+#include <stdio.h>  /* printf üçün */
 
 void print_strings(const char *separator, const unsigned int n, ...);
 
